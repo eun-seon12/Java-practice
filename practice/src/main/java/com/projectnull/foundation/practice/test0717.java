4297e0227962737c2f5cf69c11efba0482ef3f60
DESKTOP-3OSO63K\os <ces234021@gmail.com>
2025-07-17 23:10:19 +0900

    250717 practice revise num 12

--- practice/src/main/java/com/projectnull/foundation/practice/test0717.java
+++ practice/src/main/java/com/projectnull/foundation/practice/test0717.java
@@ -90,18 +90,18 @@ public class test0717 {
         // 문제 12.
 
         System.out.print("성실 점수 : ");
-        int snum = sc.nextInt();
+        int diligent = sc.nextInt();
         System.out.print("서비스 점수 : ");
         int service = sc.nextInt();
         System.out.print("미소 점수 : ");
         int smile = sc.nextInt();
 
-        int avg = (snum+service+smile)/3;
+        int avg = (diligent+service+smile)/3;
 
         if (avg>=60){
-            if (snum>=40 && service>=40 && smile>=40){
+            if (diligent>=40 && service>=40 && smile>=40){
                 System.out.println("합격입니다.");
-            } else if (snum<40) {
+            } else if (diligent<40) {
                 System.out.println("성실 점수 미달로 불합격입니다.");
             } else if (service<40) {
                 System.out.println("서비스 점수 미달로 불합격입니다.");
