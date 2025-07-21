356c0cdff6f0eb202037ea6a84df626cb6b2835d
I7E-72\i7E-63 <ces234021@gmail.com>
2025-07-21 17:16:00 +0900

    250721 test

---
+++ practice/method-practice/src/main/java/com/ohgiraffers/pratice01/Application.java
@@ -0,0 +1,29 @@
+package com.ohgiraffers.pratice01;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        int num1=10;
+        int num2=20;
+        int num3=5;
+
+        Calculator calculator = new Calculator();
+
+        calculator.checkMethod();
+        System.out.println("메소드 호출 확인");
+
+        int sum1to10 = calculator.sum1to10();
+        System.out.println("1부터 10까지의 합 : "+sum1to10);
+
+        int max = calculator.checkMaxNumber(num1, num2);
+        System.out.println("두 수 중 큰 수는 "+ max +"이다.");
+
+        int sum= calculator.sumTwoNumber(num1,num2);
+        System.out.println(num1+"과 "+num2+"의 합은 : "+sum);
+
+        int minus = calculator.minusTwoNumber(num1, num3);
+        System.out.println(num1+"과 "+num3+"의 차는 : "+minus);
+
+    }
+}
---
+++ practice/method-practice/src/main/java/com/ohgiraffers/pratice01/Calculator.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.pratice01;
+
+public class Calculator {
+
+    public void checkMethod() {
+    }
+
+    public int sum1to10(){
+        return (1+2+3+4+5+6+7+8+9+10);
+    }
+
+    public int checkMaxNumber(int num1, int num2){
+        return (num1<num2)?num2 : num1;
+    }
+
+    public int sumTwoNumber(int num1, int num2){
+        return (num1+num2);
+    }
+
+    public int minusTwoNumber(int num1, int num3){
+        return (num1-num3);
+    }
+}
