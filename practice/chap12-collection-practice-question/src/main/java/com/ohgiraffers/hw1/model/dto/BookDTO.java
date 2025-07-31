efc3e411a5c2b42ca6c58f12ead4fd5d3da891af
DESKTOP-3OSO63K\os <ces234021@gmail.com>
2025-07-31 23:24:44 +0900

    0731

--- practice/chap12-collection-practice-question/src/main/java/com/ohgiraffers/hw1/model/dto/BookDTO.java
+++ practice/chap12-collection-practice-question/src/main/java/com/ohgiraffers/hw1/model/dto/BookDTO.java
@@ -7,7 +7,8 @@ public class BookDTO {
     private String title;
     private String author;
 
-    public BookDTO() {}
+    public BookDTO() {
+    }
 
     public BookDTO(int bNo, int category, String title, String author) {
         this.bNo = bNo;
@@ -17,22 +18,27 @@ public class BookDTO {
     }
 
     public int getbNo() {
+
         return bNo;
     }
 
     public void setbNo(int bNo) {
+
         this.bNo = bNo;
     }
 
     public int getCategory() {
+
         return category;
     }
 
     public void setCategory(int category) {
+
         this.category = category;
     }
 
     public String getTitle() {
+
         return title;
     }
 
@@ -41,16 +47,22 @@ public class BookDTO {
     }
 
     public String getAuthor() {
+
         return author;
     }
 
     public void setAuthor(String author) {
+
         this.author = author;
     }
 
     @Override
     public String toString() {
         return "BookDTO{" +
+                "도서 번호 : " + bNo +
+                ", 도서 장르 : " + category +
+                ", 도서 제목 : " + title +
+                ", 도서 저자 : " + author +
                 '}';
     }
 }
